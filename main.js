ff430e04e9a1889671f45c701f60d6b4ee600be8
ScrambledMC <117959999+ScrambledMC@users.noreply.github.com>
2024-10-22 09:43:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,64 +1,5 @@
 try {
-    class Polygon {
-        constructor(points) {
-            this.points = points;
-        }
-
-        
-        render() {
-            beginShape();
-            for (var i of this.points)
-                vertex(i.x, i.y);
-
-            vertex(this.points[0].x, this.points[0].y);
-            endShape();
-        }
-
-
-        pointInLine(point, index) {
-            var point1 = this.points[index];
-            var point2 = this.points[(index + 1) % this.points.length];
-            var point3 = this.points[(index + 2) % this.points.length];
-
-            var slope = (point2.y - point1.y) / (point2.x - point1.x);
-            var y = x => slope * (x - point1.x) + point1.y;
-
-            return (y(point3.x) - point3.y) * (y(point.x) - point.y);
-        }
-
-
-        pointInPolygon(point) {
-            var state = 1;
-            for (var i = 0; i < this.points.length; i ++) {
-                var currentState = this.pointInLine(point, i);
-                if (currentState === 0)
-                    state = 0;
-                if (currentState < 0)
-                    return -1;
-            }
-
-            return state;
-        }
-
-
-        polygonInPolygon(polygon) {
-            var state = -1;
-
-            for (var i of this.points) {
-                var currentState = polygon.pointInPolygon(i);
-                if (currentState > state)
-                    state = currentState;
-            }
-
-            for (var i of polygon.points) {
-                var currentState = this.pointInPolygon(i);
-                if (currentState > state)
-                    state = currentState;
-            }
-
-            return state;
-        }
-    }
+    var keys = {};
 
 
     var pentagon = new Polygon([
@@ -69,25 +10,60 @@ try {
         {x: 1000, y: 600}
     ]);
 
-    var tr1angle = new Polygon([
-        {x: 1200, y: 700},
-        {x: 1400, y: 1100},
-        {x: 850, y: 850}
-    ]);
 
-
-    alert(pentagon.polygonInPolygon(tr1angle));
-    // 1 if colliding, 0 if on line, -1 if not colliding
+    var player = new Player(
+        {x: 800, y: 500},
+        0,
+        new Polygon([
+            {x: 0, y: -200},
+            {x: 200, y: 200},
+            {x: -150, y: -50}
+        ])
+    );
 
     
+    function drawThings() {
+        background(255, 255, 255);
+        pentagon.render();
+        player.render();
+
+
+        fill(0);
+        textSize(20);
+        text(player.getPolygon().polygonInPolygon(pentagon), 0, 40);
+        // 1 if colliding, 0 if on line, -1 if not colliding
+    }
+
+
+    function keyPressed() {
+        keys[keyCode] = true;
+    }
+
+    function keyReleased() {
+        keys[keyCode] = false;
+    }
+
+
     function setup() {
         createCanvas(2000, 1200);
     }
 
     function draw() {
-        noFill();
-        pentagon.render();
-        tr1angle.render();
+        if (keys[87])
+            player.xyz.y -= 5;
+        if (keys[83])
+            player.xyz.y += 5;
+        if (keys[65])
+            player.xyz.x -= 5;
+        if (keys[68])
+            player.xyz.x += 5;
+        if (keys[81])
+            player.rotation -= 0.05;
+        if (keys[69])
+            player.rotation += 0.05;
+        //w: 87, s: 83, a: 65, d: 68, q: 81, e: 69
+        
+        drawThings();
     }
 
 } catch (e) {
